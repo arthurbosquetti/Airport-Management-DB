3ecf074020cf2043efb014075cc09e7b148b4a05
Marek Igor Fijolek <s214036@nnfcb-l0968.students.clients.local>
2023-03-23 10:57:47 +0100

    Fixed the script

--- 40_02170DatabaseScript1_2023.sql
+++ 40_02170DatabaseScript1_2023.sql
@@ -15,32 +15,39 @@ drop table if exists Airports;
 
 SET FOREIGN_KEY_CHECKS=1;
 
-# Gate
-create table Gate(
-	GateId varchar(3) not null,
-	FlightID varchar(10),
-	AllocationStart Time,
-	AllocationEnd Time,
-	FloorLevel decimal(1,0),
-    Terminal varchar(1) not null,
-	constraint id_format check (GateId like '[A-Z][0-9][0-9]'),
-	foreign key(Terminal) references Terminal(TerminalID),
-    foreign key(FlightID) references Flights(FlightID),
-    primary key(GateId, Terminal)
-	);
-    
 # Terminal
 create table Terminal(
-TerminalID char(1),
-constraint id_format check (TerminalID like '[1-9]'),
-primary key(TerminalID)
-);
+    TerminalID char(1) not null,
+    constraint id_format check (TerminalID like '[1-9]'),
+    primary key(TerminalID)
 
+    );
+
+# Person
+create table Passenger
+	(PassportID char(9),
+    FirstName varchar(20) not null,
+    LastName varchar(20) null,
+    Email varchar(35) not null,
+    constraint email_format check (Email like '%@%.%'),
+    Phone decimal(8,0),
+    primary key(PassportID)
+    );
+
+# Airport
+create table Airports
+	(AirportCode char(3),
+    constraint code_format check (AirportCode like '[A-Z]*'),
+    AirportName varchar(20),
+    Country varchar(20) not null,
+    City varchar(20),
+    primary key(AirportCode)
+    );
 
 # Place
 create table Place
 	(PlacesID varchar(4),
-    Terminal varchar(1),
+    Terminal char(1),
     Service varchar(4),
     foreign key(Terminal) references Terminal(TerminalID),
     primary key (PlacesID)
@@ -52,43 +59,11 @@ create table Activity
     Service varchar(4),
     ActivityDescription varchar(40),
     Place varchar(4) not null,
-    Person varchar(20) not null,
+    Person char(9) not null,
     foreign key(Place) references Place(PlacesID),
-    foreign key(Person) references Person(PassportID),
+    foreign key(Person) references Passenger(PassportID),
     primary key(ActivityID)
 	);
-    
-# Person
-create table Passenger
-	(PassportID char(9),
-    FirstName varchar(20) not null,
-    LastName varchar(20) null,
-    Email varchar(35) not null,
-    constraint email_format check (Email like '%@%.%'),
-    Phone decimal(8,0),
-    primary key(PassportID)
-    );
-    
-create table Luggage
-	(LuggageID decimal(8,0),
-    Weight decimal (4,2) not null,
-    Delivered bool,
-    OwnerID char(9) not null,
-    Ticket varchar(8) not null,
-    primary key(LuggageID),
-    foreign key(Ticket) references Ticket(TicketID)
-    );
-
-create table Ticket
-	(TicketID varchar(20),
-    Class ENUM('First class','Gold', 'Member', 'Economy'),
-    Passenger varchar(20) not null,
-    FlightID varchar(10) not null,
-    TimeSlot datetime,
-    foreign key(Passenger) references Person(PassportID),
-    foreign key(FlightID) references Flights(FlightID),
-    primary key(TicketID)
-    );
 
 create table Flights
 	(FlightID varchar(10) not null,
@@ -101,16 +76,42 @@ create table Flights
     SourceCode char(3),
     DestinationCode char(3),
     primary key(FlightID, LocalDate),
-    foreign key(SourceCode) references Airport(AirportCode)
+    foreign key(SourceCode) references Airports(AirportCode)
     );
 
-create table Airports
-	(AirportCode char(3),
-    constraint code_format check (AirportCode like '[A-Z]*'),
-    AirportName varchar(20),
-    Country varchar(20) not null,
-    City varchar(20),
-    primary key(AirportCode)
+# Gate
+create table Gate(
+	GateId varchar(3) not null,
+	FlightID varchar(10) not null,
+	AllocationStart Time,
+	AllocationEnd Time,
+	FloorLevel decimal(1,0),
+    Terminal char(1) not null,
+	constraint id_format check (GateId like '[A-Z][0-9][0-9]'),
+	foreign key(Terminal) references Terminal(TerminalID),
+    foreign key(FlightID) references Flights(FlightID),
+    primary key(GateId, Terminal)
+	);
+
+create table Ticket
+	(TicketID varchar(20),
+    Class ENUM('First class','Gold', 'Member', 'Economy'),
+    Passenger char(9) not null,
+    FlightID varchar(10) not null,
+    TimeSlot datetime,
+    foreign key(Passenger) references Passenger(PassportID),
+    foreign key(FlightID) references Flights(FlightID),
+    primary key(TicketID)
+    );
+    
+create table Luggage
+	(LuggageID decimal(8,0),
+    Weight decimal (4,2) not null,
+    Delivered bool,
+    OwnerID char(9) not null,
+    Ticket varchar(20) not null,
+    primary key(LuggageID),
+    foreign key(Ticket) references Ticket(TicketID)
     );
     
     
